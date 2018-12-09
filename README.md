f2159da34ed4742fec168b7d68d151414b1b9cde
Ryan Tolboom <Ryan.Tolboom@monroe.k12.nj.us>
2018-12-08 22:26:24 -0500

    Added analysis and info about presentation

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Predicting Website Navigation with MapReduce in Hadoop
 
+Presentation can be found
+[here](https://docs.google.com/presentation/d/1XSUvSYeoF0-qAdLf9mmImw0EtSuDcG2uYQrUlsExKPQ/edit?usp=sharing).
+
 ## Dataset
 
 This project uses the English
@@ -46,4 +49,13 @@ mvn clean install
 hadoop jar target/click-stream-0.0.1.jar hadoop.NextClick src/main/resources/clickstream-enwiki-2018-10-abridged.tsv
 ```
 
-### Script Analysis (WIP)
+### Script Analysis
+
+A python script will generate breadcrumb trails for all Wikipedia pages in
+compressed TSV format. It will also create a histogram of breadcrumb trail
+length. It reads results/next_click.tsv.gz (copy from output/next_click/part*)
+and writes to results/breadcrumbs_histogram.png and results/breacrumbs.tsv.gz.
+Numpy and matplotlib are requried for the analysis.
+```bash
+python3 scripts/breadcrumbs.py
+```
--- scripts/breadcrumbs.py
+++ scripts/breadcrumbs.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-
-import csv
+"""
+Outputs a file with the most common breadcrumb trails for all wikipedia pages
+Output TSV Format: length_of_trail page trail
+Outputs a histogram of breadcrumb trail length in PNG format
+"""
+import csv, gzip
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
 
 next_click = {}
+lengths = []
 
 def follow(link, breadcrumbs):
     if link in breadcrumbs or link not in next_click:
@@ -11,12 +19,24 @@ def follow(link, breadcrumbs):
         breadcrumbs.append(link);
         return follow(next_click[link], breadcrumbs);
 
-with open('results/next_click.tsv', newline='') as csvfile:
+with gzip.open('results/next_click.tsv.gz', mode='rt', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter='\t')
     for row in reader:
         next_click[row[0]] = row[1]
 
-for link in next_click:
-    breadcrumbs = follow(link, [])
-    path = "->".join(breadcrumbs)
-    print("{}: {}".format(len(breadcrumbs), path))
+with gzip.open('results/breadcrumbs.tsv.gz', mode='wt', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter='\t')
+    for page in next_click:
+        breadcrumbs = follow(page, [])
+        length = len(breadcrumbs)
+        lengths.append(length)
+        trail = "->".join(breadcrumbs)
+        writer.writerow([length, page, trail])
+
+plt.hist(lengths, bins=range(300), alpha=0.75)
+plt.xlabel('Length of Breadcrumb Trail')
+plt.ylabel('Frequency')
+plt.title('Histogram of Wikipedia Breadcrumbs')
+plt.axis([0, 15, 0, 4e5])
+plt.tight_layout()
+plt.savefig('results/breadcrumbs_histogram.png')
