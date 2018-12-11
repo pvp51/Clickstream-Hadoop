06a268316662a47ef4b34afb9188bcc9f5253fb6
Parth <parth5494@live.com>
2018-12-10 21:59:42 -0500

    type count code completed

--- src/main/java/hadoop/TypeCount.java
+++ src/main/java/hadoop/TypeCount.java
@@ -1,6 +1,8 @@
 package hadoop;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -8,55 +10,50 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.Reducer.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class TypeCount {
 
-	 //maps the Wikipedia Clickstream input to K(prev) V(curr n) 
-		public static class TokenizerMapper extends
-	        Mapper<Object, Text, Text, Text> {
+	//maps the Wikipedia Clickstream input to K(prev) V(curr n) 
+	public static class TokenizerMapper extends
+	Mapper<Object, Text, Text, Text> {
 
-			public void map(Object key, Text value, Context context) throws
-	            IOException, InterruptedException {
+		public void map(Object key, Text value, Context context) throws
+		IOException, InterruptedException {
 
-	            String[] items = value.toString().split("\t");
-	            String type = items[2];
-	            int n = Integer.parseInt(items[3]);
-	                
-	            context.write(new Text(type), new Text(String.format("%s\t%d",
-	                type, n)));
-			}
+			String[] items = value.toString().split("\t");
+			String type = items[2];
+			context.write(new Text(type), new Text(type));
 		}
+	}
 
-	    //reduces set to just the number #1 click for a prev value
-	    public static class NextClickReducer extends
-	        Reducer<Text, Text, Text, Text> {
+	//reduces set to just the number #1 click for a prev value
+	public static class TyepCountReducer extends
+	Reducer<Text, Text, Text, Text> {
+		Map<String, Integer> typeCount = new HashMap<>();
 
-	    	public void reduce(Text key, Iterable<Text> values,
-	                Context context) throws IOException, InterruptedException {
+		public void reduce(Text key, Iterable<Text> values,
+				Context context) throws IOException, InterruptedException {
+			
+			int recordCount = 0;
+			String count = "";
+			
+			for(@SuppressWarnings("unused") Text val : values){
+				recordCount++;
+			}
+			count = ""+recordCount;
+			context.write(key, new Text(count));
+		}
+	}
 
-	        	 int recordCount = 0;
-	        	 String count = "";
-	             for (Text value : values) {
-	                 String[] items = value.toString().split("\t");
-	                 String type = items[2];
-	                 if (type.equalsIgnoreCase(key.toString())) {
-	                     count = "" +recordCount++;
-	                 }
-	             }
-	             context.write(key, new Text(count));
-	        }
-	    }
-	    
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 		Configuration conf = new Configuration();
 
 		Job job = Job.getInstance(conf, "Type Count");
 		job.setJarByClass(NextClick.class);
 		job.setMapperClass(TokenizerMapper.class);
-		job.setReducerClass(NextClickReducer.class);
+		job.setReducerClass(TyepCountReducer.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
 		FileInputFormat.addInputPath(job, new Path(args[0]));
